e8dc21bada7d6b942d4ab22b2b1dfde6080c8209
joneju <julian.jones@yahoo.com>
2025-09-02 12:01:26 -0700

    Creating Repository.

---
+++ src/main/java/mc/lotcFarmingPluginTest/LotcFarmingPluginTest.java
@@ -0,0 +1,17 @@
+package mc.lotcFarmingPluginTest;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LotcFarmingPluginTest extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
