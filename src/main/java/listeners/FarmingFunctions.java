fd6432d12483abc36244b39033c5e6f00128e5a6
joneju <julian.jones@yahoo.com>
2025-09-03 15:51:10 -0700

    Fixed the issue with Pumpkins/Melons, and fleshed out the current TODO lists.

--- src/main/java/listeners/FarmingFunctions.java
+++ src/main/java/listeners/FarmingFunctions.java
@@ -13,29 +13,25 @@ import org.bukkit.inventory.ItemStack;
 import scheduleHandler.handler;
 
 //TODO:
-// Update the "Harvest" function to do some extra looking into Cactus and Sugarcane to prevent an infinite glitch.
-// For Sugarcane and Cactus, make sure to also check the NBT of the block to make sure the block was NOT placed
-//  by a player. If placed by player, only give 1 block in return. This will be the origin tag.
+// If CoreProtect is added, Update the "harvestActivity" method to do some extra looking into Cactus, Sugarcane,
+// Pumpkins, and Melons to prevent an infinite glitch. If the block was placed by a player, make it only ever drop 1
+// item/seed, regardless of enchants on the harvesting item.
 
-public class FarmingFunctions implements Listener{
-    // Make a 'Harvest' function that activates when right-clicking a crop
-    // When right-clicking the crop, check to see what item the player is holding and pass it and the crop
-    //   to the 'Harvestable' function.
-    // If the 'Harvestable' function returns FALSE, do nothing. Else, complete harvestActivity function.
-
+public class FarmingFunctions implements Listener {
     @EventHandler
     public void harvestActivity(PlayerInteractEvent e) {
+        // Main method to harvest a farmable block.
         if (e.getAction() == Action.RIGHT_CLICK_BLOCK) {
-            Player user = e.getPlayer(); // Get player
-            // Get the block that was clicked, and what its type is.
+            // Collect info on the player, the block they clicked on, and what tool they are holding (if any).
+            Player user = e.getPlayer();
             Block block = e.getClickedBlock() != null ? e.getClickedBlock() : null;
-            // Get the tool that clicked on the block.
             ItemStack tool = e.getItem();
+
             // Check Harvestability, assuming that there is an item in hand and a block being aimed at.
             //   If the block is harvestable, harvest it, and replant if possible.
             if (tool != null && block != null) {
                if (isCrop(block)) {
-                    harvestCrop(tool, block);
+                    harvestCrop(tool, block, user);
                }
             }
         }
@@ -44,35 +40,52 @@ public class FarmingFunctions implements Listener{
     public boolean isCrop (Block block) {
         return switch (block.getType()) {
             case Material.CARROTS, Material.POTATOES, Material.BEETROOTS, Material.WHEAT, Material.NETHER_WART,
-                 Material.COCOA_BEANS, Material.SUGAR_CANE, Material.CACTUS, Material.MELON, Material.PUMPKIN,
-                 Material.BAMBOO -> true;
+                 Material.COCOA_BEANS, Material.SUGAR_CANE, Material.CACTUS, Material.MELON, Material.PUMPKIN -> true;
             default -> false;
         };
     }
 
-    public void harvestCrop (ItemStack item, Block clickedBlock) {
+    public void harvestCrop (ItemStack item, Block clickedBlock, Player user) {
+        // This method will harvest a block, assuming the correct conditions are met, which can be inferred by
+        //  reading the below code and comments.
         boolean isHoe = item.getType().name().endsWith("_HOE");
         boolean isAxe = item.getType().name().endsWith("_AXE");
-        Material newCropType = clickedBlock.getType();
+
+        // Create a new crop block. Default age = 0, so no adjustment needed.
         BlockData newCrop = clickedBlock.getType().createBlockData();
-        if (newCrop instanceof Ageable ageable) { ageable.setAge(0); }
-        boolean isMature = ((Ageable) clickedBlock.getBlockData()).getAge() == ((Ageable) clickedBlock.getBlockData()).getMaximumAge();
+        System.out.println("Material is: " + clickedBlock.getType());
         switch (clickedBlock.getType()) {
+            // Check block types for valid crops.
             case Material.CARROTS, Material.POTATOES, Material.BEETROOTS, Material.WHEAT, Material.NETHER_WART,
                  Material.COCOA:
-                if (isHoe && isMature) {
+                // For certain crops like the ones in this case, they must be checked for maturity before harvesting.
+                if (isHoe && (((Ageable) clickedBlock.getBlockData()).getAge() == ((Ageable) clickedBlock.getBlockData()).getMaximumAge())) {
                     clickedBlock.breakNaturally();
                     handler.placeBlock(clickedBlock, newCrop);
                 } else {
+                    if (!isHoe) {
+                        user.sendMessage("This is the wrong tool to harvest with...");
+                    } else {
+                        user.sendMessage("This crop is not mature yet!");
+                    }
                     break;
                 }
+                break;
+
             case Material.SUGAR_CANE, Material.CACTUS:
                 if (isHoe) {
                     clickedBlock.breakNaturally();
+                } else {
+                    user.sendMessage("This is the wrong tool to harvest with...");
+                    break;
                 }
-            case Material.MELON, Material.PUMPKIN, Material.BAMBOO:
+
+            case Material.MELON, Material.PUMPKIN:
                 if (isAxe) {
                     clickedBlock.breakNaturally();
+                } else {
+                    user.sendMessage("This is the wrong tool to harvest with...");
+                    break;
                 }
         }
     }
--- src/main/java/mc/lotcFarmingPluginTest/LotcFarmingPluginTest.java
+++ src/main/java/mc/lotcFarmingPluginTest/LotcFarmingPluginTest.java
@@ -6,14 +6,32 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import scheduleHandler.handler;
 
-// TODO:
-// 1.0 Start adjustment on crop drop table.
-
-// 3.1 Update the harvest check: If the incorrect tool is being used, do nothing and print "This is the wrong tool to harvest with!"
-//     If the correct tool is used but the crop is not mature, do nothing and print "This crop is not yet ready to harvest."
-//
-// 4.0 Alter the crop break event to make it drop no crops or seeds if broken by anything other than the correct tool
-// 4.1 This includes things like pistons, crop trampling, darkness, water, explosions, or a piston moving the supporting block.
+//TODO:
+// - 1.0: Start adjustment on crop drop table.
+// - 1.1: If harvested with a wooden/stone tool, drop 1 crop and 1 seed. 2 crops for carrots/potatoes.
+// - 1.2. If harvested with an iron/gold tool, drop 2 crops and 1 seed. 3 crops for carrots/potatoes.
+// - 1.3. If harvested with a diamond/netherite tool, drop 3 crops and 1 seed. 4 crops for carrots/potatoes.
+// - 2.0: Adjust how enchantments work on tools, and make it so breaking crops with any tool/weapon does nothing and
+//        removes no durability, unless it's right clicked by the correct tool.
+// - 2.1: Unbreaking 1 should increase durability by 1.5x, and further levels by another 0.5
+//        That means Unbreaking 1 = 1.5x, Unbreaking 2 = 2x, Unbreaking 3 = 2.5x, etc. It's basically (0.5*level + 1)x.
+//        Which can work for plugins that increase enchantment levels, maybe.
+// - 2.2: Each level of Fortune should increase the crop/seed drop by 1. Crops remain the same, but add an extra crop
+//        per 2 seeds if the crop is its own seed, which are carrots, potatoes, nether wart, sugarcane, cactus, cocoa,
+//        and pumpkins.
+//        This means (sc = seed-crop):
+//        - Fortune 1 = 2 crop 2 seed (3 crop sc)
+//        - Fortune 2 = 3 crop 3 seed (4 crop sc)
+//        - Fortune 3 = 4 crop 4 seed (6 crop sc)
+//        Note: for the calculation on seeds just use modulo 2 to get an easy full number.
+// - 2.3 The only exception to these tables I imagine is for Melons. Melons naturally give 4-6 per block, so we'll do 4
+//       naturally, and add half of the current per level, rounded down.
+//       - Fortune 1 ~ 6
+//       - Fortune 2 ~ 9
+//       - Fortune 3 ~ 13
+// - 3.0 Alter the crop break event to make it only ever drop 1 crop/seed if somehow broken by anything other than the
+//       correct tool. Such things that can do this are pistons, crop trampling, darkness, water, lava, explosions,
+//       or a piston moving the supporting block. Mob griefing should be noted to also cause explosions and trampling.
 
 public final class LotcFarmingPluginTest extends JavaPlugin {
 
